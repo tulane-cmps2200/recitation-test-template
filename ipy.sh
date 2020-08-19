620a2c7d74967d933deb1a35d01ab84bb99b58cf
Aron <aronwc@gmail.com>
2020-08-19 17:28:03 -0500

    paths

--- ipy.sh
+++ ipy.sh
@@ -1,1 +1,1 @@
-python3 -c "import pytest" ; if [ $? -eq 1 ]; then ./setup.sh; fi; ipython3 -i init.ipy
+export PATH=$PATH:/home/runner/.local/bin; python3 -c "import pytest" ; if [ $? -eq 1 ]; then ./setup.sh; fi; ipython3 -i init.ipy
--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 wget "https://bootstrap.pypa.io/get-pip.py"
 python3 get-pip.py
+export PATH=$PATH:/home/runner/.local/bin
 pip3 install -r requirements.txt
